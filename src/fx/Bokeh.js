bf11ba7657a952f70dfc7807f551046b968bfab5
Chris Neale <chris@komododigital.co.uk>
2018-09-12 14:12:29 +0100

    Removed unnecessary bokeh functions:

--- src/fx/Bokeh.js
+++ src/fx/Bokeh.js
@@ -2,22 +2,11 @@ import Fx from "../Fx";
 
 export default class Bokeh extends Fx {
 
-    constructor() {
-        super();
-        this.mouse = [0, 0];
-    }
-
     draw() {
         if (this.ctx!==null) {
 
         }
         this.raf = requestAnimationFrame(this.draw);
-    }
-
-    listeners(el) {
-        el.addEventListener('mousemove', (e) => {
-            this.mouse = [e.x - this.bb.left, e.y - this.bb.top];
-        })
     }
 
 }
