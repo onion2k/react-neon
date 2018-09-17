6260dc7d2e722bf4fe6f0e6c3675014bc54d8b49
Chris Neale <chris@komododigital.co.uk>
2018-09-17 16:57:36 +0100

    Updated light to try rotation.

--- src/fx/Light.js
+++ src/fx/Light.js
@@ -11,7 +11,6 @@ export default class Light extends Fx {
         if (this.ctx!==null) {
 
             this.ctx.setTransform(1,0,0,1,0,0);
-
             this.ctx.clearRect(0, 0, this.bb.width, this.bb.height);
 
             this.ctx.strokeStyle = 'hsla(0, 100%, 0%, 1)';
@@ -19,14 +18,21 @@ export default class Light extends Fx {
             this.ctx.moveTo(this.bb.width / 2, this.bb.height / 2);
             this.ctx.lineTo(this.mouse[0], this.mouse[1]);
             this.ctx.stroke();
-
+            
             this.ctx.setTransform(1, 0, 0, 0.5, 0, this.bb.height / 4);
 
             let g = this.ctx.createRadialGradient(this.bb.width / 2, this.bb.height / 2, 0, this.bb.width / 2, this.bb.height / 2, this.bb.width / 2);
-            g.addColorStop(0, 'hsla(0,100%,50%,1)');
-            g.addColorStop(1, 'hsla(0,100%,50%,0)');
+            g.addColorStop(0, 'hsla(0,100%,100%,0)');
+            g.addColorStop(1, 'hsla(0,100%,0%,1)');
             this.ctx.fillStyle = g;
             this.ctx.fillRect(0, 0, this.bb.width, this.bb.height);
+
+            this.ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+            // this.ctx.translate(-1 * this.bb.width / 2, -1 * this.bb.height / 2);
+            // this.ctx.rotate( 45 * (Math.PI / 180) );
+            // this.ctx.translate(this.bb.width / 2, this.bb.height / 2);
+
 
         }
         this.raf = requestAnimationFrame(this.draw);
