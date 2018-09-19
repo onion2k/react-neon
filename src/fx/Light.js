3aa5acfeac81ff73b1bbe7bdc12926187166a0b4
Chris Neale <chris@komododigital.co.uk>
2018-09-19 13:28:45 +0100

    Working light

--- src/fx/Light.js
+++ src/fx/Light.js
@@ -10,37 +10,23 @@ export default class Light extends Fx {
     draw() {
         if (this.ctx!==null) {
 
-            this.ctx.setTransform(1,0,0,1,0,0);
             this.ctx.clearRect(0, 0, this.bb.width, this.bb.height);
 
-            this.ctx.strokeStyle = 'hsla(0, 100%, 0%, 1)';
-            this.ctx.beginPath();
-            this.ctx.moveTo(this.bb.width / 2, this.bb.height / 2);
-            this.ctx.lineTo(this.mouse[0], this.mouse[1]);
-            this.ctx.stroke();
-            
-            this.ctx.setTransform(1, 0, 0, 1, 0, 0);
-            // this.ctx.rotate( 30 * (Math.PI / 180) );
+            const lx = ((this.mouse[0] / this.bb.width) * 2 - 1);
+            const ly = ((this.mouse[1] / this.bb.height) * 2 - 1);
 
             let g = this.ctx.createRadialGradient(
-                this.bb.width / 2,
-                this.bb.height / 2,
+                (this.bb.width / 2),
+                (this.bb.height / 2),
                 0,
-                (this.bb.width - this.mouse[0]),
-                (this.bb.height - this.mouse[1]),
+                ( (this.bb.width / 2) + lx * (this.bb.width / 3) ),
+                ( (this.bb.height / 2) + ly * (this.bb.height / 3) ),
                 this.bb.width / 2
             );
             g.addColorStop(0, 'hsla(0,100%,100%,0)');
             g.addColorStop(1, 'hsla(0,100%,0%,1)');
             this.ctx.fillStyle = g;
             this.ctx.fillRect(0, 0, this.bb.width, this.bb.height);
-
-            // this.ctx.fillStyle = 'hsla(0,100%,0%,1)';
-            // this.ctx.fillRect(0, 0, this.bb.width , this.bb.height );
-            // this.ctx.fillRect(0, this.bb.height  - 1, this.bb.width, this.bb.height + 1);
-
-            this.ctx.setTransform(1, 0, 0, 1, this.bb.width / 2, this.bb.height / 2);
-            // this.ctx.translate(this.bb.width, this.bb.height);
 
         }
         this.raf = requestAnimationFrame(this.draw);
