a7c9242b242b84494faffd36dadb8b519ea59968
Chris Neale <chris@komododigital.co.uk>
2018-09-20 12:29:32 +0100

    Added snow wind factor

--- src/fx/Snow.js
+++ src/fx/Snow.js
@@ -4,19 +4,16 @@ export default class Snow extends Fx {
 
     constructor() {
         super();
-        this.maxActive = 1000;
+        this.wind = 2;
+        this.maxActive = 200;
         this.snowflakes = [];
     }
 
     init() {
-
         for (var i=0; i<this.maxActive; i++) {
-
             var x = Math.floor(Math.random() * this.bb.width);
             var y = Math.floor(Math.random() * this.bb.height);
-
             this.snowflakes.push({ x: x, y: y, vy: 1 + (Math.random()*3) });
-
         }
     }
 
@@ -29,13 +26,17 @@ export default class Snow extends Fx {
                 this.snowflakes.forEach((f)=>{
                     f.y += f.vy; 
                     if (f.y > this.bb.height){
+                        f.x = Math.floor(Math.random() * this.bb.width);
                         f.y = 0;
+                    }
+                    if (f.x > this.bb.width){
+                        f.x = 0;
                     }
                     this.ctx.fillStyle = 'hsla(0,100%,100%,1)';
                     this.ctx.beginPath();
                     this.ctx.arc(f.x, f.y, 2, 0, 2 * Math.PI);
                     this.ctx.fill();
-                    f.x +=  Math.random() * 2 - 1;
+                    f.x += this.wind + (Math.random() * 2 - 1);
                 });    
             }
 
