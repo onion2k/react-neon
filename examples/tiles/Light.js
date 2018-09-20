3bfca121e09c1190c98cdbfd06366e136cf256f4
Chris Neale <chris@komododigital.co.uk>
2018-09-20 08:42:10 +0100

    Better light calc

--- examples/tiles/Light.js
+++ examples/tiles/Light.js
@@ -5,15 +5,12 @@ import withNeon, { fx } from "../../src/index.js";
 class LightTile extends React.Component {
   render(){
     return (
-      <Tile bgIm="https://source.unsplash.com/random?l" />
+      <Tile bgIm="https://source.unsplash.com/featured/?night" />
     )
   }
 };
 
-const lightConfig = {
-  type: "light"
-};
-
-const effect = new fx.Light(lightConfig);
+const effect = new fx.Light({ startColor: 'hsla(0,100%,100%,0)', endColor: 'hsla(0,100%,0%,1)',  });
+effect.listenMouse();
 
 export default withNeon(LightTile, effect);
--- src/fx/Light.js
+++ src/fx/Light.js
@@ -2,10 +2,9 @@ import Fx from "../Fx";
 
 export default class Light extends Fx {
 
-    constructor() {
-        super();
-        this.mouse = [0, 0];
-    }
+    clamp(x, min, max) {
+        return Math.min(Math.max(x, min), max);
+    };
 
     draw() {
         if (this.ctx!==null) {
@@ -15,27 +14,27 @@ export default class Light extends Fx {
             const lx = ((this.mouse[0] / this.bb.width) * 2 - 1);
             const ly = ((this.mouse[1] / this.bb.height) * 2 - 1);
 
+            const halfWidth = (this.bb.width / 2);
+            const thirdWidth = (this.bb.width / 3);
+
+            const halfHeight = (this.bb.height / 2);
+            const thirdHeight = (this.bb.height / 3);
+
             let g = this.ctx.createRadialGradient(
-                (this.bb.width / 2),
-                (this.bb.height / 2),
+                halfWidth,
+                halfHeight,
                 0,
-                ( (this.bb.width / 2) + lx * (this.bb.width / 3) ),
-                ( (this.bb.height / 2) + ly * (this.bb.height / 3) ),
-                this.bb.width / 2
+                halfWidth + this.clamp((lx * thirdWidth), -1 * thirdWidth, thirdWidth),
+                halfHeight + this.clamp((ly * thirdHeight), -1 * thirdHeight, thirdHeight),
+                halfHeight
             );
-            g.addColorStop(0, 'hsla(0,100%,100%,0)');
-            g.addColorStop(1, 'hsla(0,100%,0%,1)');
+            g.addColorStop(0, this.options.startColor);
+            g.addColorStop(1, this.options.endColor);
             this.ctx.fillStyle = g;
             this.ctx.fillRect(0, 0, this.bb.width, this.bb.height);
 
         }
         this.raf = requestAnimationFrame(this.draw);
-    }
-
-    listeners(el) {
-        el.addEventListener('mousemove', (e) => {
-            this.mouse = [e.x - this.bb.left, e.y - this.bb.top];
-        })
     }
 
 }
