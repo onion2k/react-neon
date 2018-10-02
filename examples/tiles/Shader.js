a00200bdc09f7594532611e4cf5fcee685dd3eb6
Chris Neale <chris@komododigital.co.uk>
2018-10-02 08:36:46 +0100

    Made shader effect transparent|

--- examples/tiles/Shader.js
+++ examples/tiles/Shader.js
@@ -10,25 +10,29 @@ class ShaderTile extends React.Component {
   }
 };
 
-const vs = `attribute vec4 position;
-void main() {
-  gl_Position = position;
-}`;
+const vs = `
+  attribute vec4 position;
+  void main() {
+    gl_Position = position;
+  }
+`;
 
-const fs = `#ifdef GL_ES
-precision mediump float;
-#endif
+const fs = `
+  #ifdef GL_ES
+  precision mediump float;
+  #endif
 
-uniform vec2 u_resolution;
-uniform float u_time;
+  uniform vec2 u_resolution;
+  uniform float u_time;
 
-void main()
-{
-    vec2 uv = gl_FragCoord.xy / u_resolution;
-    uv = uv + vec2(0.5 - cos(uv.y * 8.0), 2.0) * (sin(u_time) * 0.05);
-    float cb = floor(uv.x*25.) + floor(uv.y*25.);
-    gl_FragColor = vec4(0.0, 0.0, 0.0,mod(cb, 2.0));
-}`
+  void main()
+  {
+      vec2 uv = gl_FragCoord.xy / u_resolution;
+      uv = uv + vec2(0.5 - cos(uv.y * 8.0), 2.0) * (sin(u_time) * 0.05);
+      float cb = floor(uv.x*25.) + floor(uv.y*25.);
+      gl_FragColor = vec4(1.0, 0.0, 0.0, 0.5 * mod(cb, 2.0));
+  }
+`
 
 const effect = new fx.Shader({ vs: vs, fs: fs });
 
